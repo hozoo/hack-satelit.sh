53ee654d624f0950f208e5bb520de48e0af17424
hozoo <143112689+hozoo@users.noreply.github.com>
2023-10-20 00:13:34 +0700

    HACK NASA MAU GESER KEMANA KETAR KETIR 😱😱😱😱😱
    😱

---
+++ Hack-Satelit.sh
@@ -0,0 +1,95 @@
+clear
+sleep 1
+
+echo "\033[31;1m
+██╗░░██╗░█████╗░███████╗░█████╗░░█████╗░
+██║░░██║██╔══██╗╚════██║██╔══██╗██╔══██╗
+███████║██║░░██║░░███╔═╝██║░░██║██║░░██║
+██╔══██║██║░░██║██╔══╝░░██║░░██║██║░░██║
+██║░░██║╚█████╔╝███████╗╚█████╔╝╚█████╔╝
+╚═╝░░╚═╝░╚════╝░╚══════╝░╚════╝░░╚════╝░"
+sleep 2
+echo "\033[32;1m========================================="
+echo "\033[34;1mSang Pembuat : HOZOO
+echo "\033[34;1mVersion : v1.0"
+echo "\033[34;1mKegunaan Tools : Memindahkan Posisi Satelit"
+echo "\033[32;1m========================================="
+echo
+sleep 2
+echo "\033[31;1m===================="
+echo "\033[34;1m[1] Hack Satelit" 
+echo "\033[33;1m[0] Kembali"
+echo "\033[31;1m===================="
+read -p "#Mr.404_FouNd ~>#" pilihan
+
+
+if [ $pilihan = "1" ] || [ $pilihan = "1" ]
+then
+clear
+sleep 2
+echo " "
+echo "\033[31;1m====================="
+echo "\033[32;1mMAU DIGESER KEMANA?"
+echo "\033[31;1m====================="
+sleep 1
+echo "\033[34;1mGESER SATELIT"
+echo "\033[35;1m<===================>"
+echo "\033[32;1m[01]. Geser Ke Kanan"
+echo "\033[35;1m<===================>"
+echo "\033[32;1m[02]. Geser Ke Kiri"
+echo "\033[35;1m<===================>"
+echo "\033[32;1m[03]. Geser Kerumah lu"
+echo "\033[35;1m<===================>"
+read -p "Silahkan Pilih :" pilih
+
+fi
+
+
+if [ $pilih = "01" ] || [ $pilih = "01" ]
+then
+sleep 2
+echo "Tungguu..."
+echo " "
+sleep 3
+echo "Sedang Geser Satelit Ke Kanan"
+echo " "
+sleep 2
+echo "\033[32;1mGeser..."
+echo " "
+sleep 5
+echo "SELESAI"
+echo " "
+echo "Nah... coba lu tengok ke kanan"
+
+fi
+
+
+if [ $pilih = "02" ] || [ $pilih = "02" ]
+then
+echo "\033[31;1mTUNGGU..."
+sleep 3
+echo "\033[32;1mGeser Geser Satelit..."
+sleep 2
+echo "\033[34;1mUdh Digeser"
+sleep 1
+echo "wihhh selesai... coba lu tengok ke kiri"
+
+fi
+
+
+if [ $pilih = "03" ] || [ $pilih = "03" ]
+then
+echo "\033[34;1mTungguu..."
+echo " "                                                  
+sleep 3
+echo "Sedang Geser Satelit Ke Rumah Elu"
+echo " "
+sleep 2
+echo "Menggeser Satelit..."
+echo " "
+sleep 5
+echo "\033[35;1mSELESAI"
+echo " "
+echo "Coba Lu liat Kedepan rumah"
+
+fi
